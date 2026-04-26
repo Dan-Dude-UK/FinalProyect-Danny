d2e62d73e35e7b64f515c764d1990c80f80a29dd
Dan-Dude-UK <ezequielareche9@gmail.com>
2026-04-25 20:10:14 -0400

    coneexion a BD

---
+++ Proyecto-Final-Danny/Models/ConexionFW.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Control_Gastos
+{
+    public class gastosContext : DbContext
+    {
+        public DbSet<Gasto> Gastos
+        {
+            get;
+            set;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MSI\\SQLEXPRESS;Database=ControlGastosDB;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ Proyecto-Final-Danny/Models/Gastos.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Control_Gastos
+{
+    public class Gasto
+    {
+        private int _id;
+        private string _descripcion;
+        private decimal _monto;
+        private DateTime _fecha;
+
+        public Gasto()
+        {
+            _id = 0;
+            _descripcion = string.Empty;
+            _monto = 0;
+            _fecha = DateTime.Now;
+        }
+
+        public Gasto(int id, string descripcion, decimal monto, DateTime fecha)
+        {
+            _id = id;
+            _descripcion = descripcion;
+            _monto = monto;
+            _fecha = fecha;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                _id = value;
+            }
+        }
+
+        public string Descripcion
+        {
+            get
+            {
+                return _descripcion;
+            }
+
+            set
+            {
+                _descripcion = value;
+            }
+        }
+
+        public decimal Monto
+        {
+            get
+            {
+                return _monto;
+            }
+
+            set
+            {
+                _monto = value;
+            }
+        }
+
+        public DateTime Fecha
+        {
+            get
+            {
+                return _fecha;
+            }
+
+            set
+            {
+                _fecha = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"[{_id}] {_descripcion} | Monto: RD${_monto} | Fecha: {_fecha.ToShortDateString()}";
+        }
+    }
+}
+
